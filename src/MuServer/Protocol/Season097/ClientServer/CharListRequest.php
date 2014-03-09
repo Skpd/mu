c5252b3c334894f0822cf61310192dda0214dd33
Skpd <dm.skpd@gmail.com>
2014-03-10 03:41:23 +0400

    added char list request packet

---
+++ src/MuServer/Protocol/Season097/ClientServer/CharListRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MuServer\Protocol\Season097\ClientServer;
+
+class CharListRequest extends AbstractPacket
+{
+    protected $class = 0xC1;
+    protected $code = 0xF3;
+    protected $subCode = 0x7A;
+}
